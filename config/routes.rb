9bbf1058c5f7433a75b9dab2718b44cf17000082
unknown <k2ca3@.(none)>
2010-01-08 11:49:59 +0900

    ルートパスの変更。 作成後のデザインの修正。

--- config/routes.rb
+++ config/routes.rb
@@ -39,10 +39,13 @@ ActionController::Routing::Routes.draw do |map|
 
   # 2ca3 add
   map.resources :hatebutas, :collection => {:hook => :post}
+  map.root :controller => 'hatebutas'
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  # 2ca3 upd 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
 end
